f1c343c9a40b55b9f7c8bbd40564c3e86794a08c
Jan Marciniec <emjan10@wp.pl>
2023-03-11 22:56:08 +0100

    CRUD

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    // protected $table = 'table_name';
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price'
+    ];
 }
